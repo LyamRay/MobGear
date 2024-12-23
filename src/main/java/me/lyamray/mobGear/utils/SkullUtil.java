2d096deb179ef1bcf89de7453c8436e7d7bc7fc8
LyamRay <lyam.ray410@gmail.com>
2024-12-23 12:58:18 +0100

    Created the project.

--- src/main/java/me/lyamray/mobGear/utils/SkullUtil.java
+++ src/main/java/me/lyamray/mobGear/utils/SkullUtil.java
@@ -9,27 +9,29 @@ import org.bukkit.profile.PlayerProfile;
 import org.bukkit.profile.PlayerTextures;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.UUID;
 
 @UtilityClass
 public class SkullUtil {
-        // Thanks JensieFlensie for this code! X
+    // Thanks JensieFlensie for this code! X
     public ItemStack getSkull(String url) {
         ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
 
         UUID uuid = UUID.nameUUIDFromBytes(url.getBytes());
 
-        PlayerProfile playerProfile = Bukkit.createPlayerProfile((uuid));
+        PlayerProfile playerProfile = Bukkit.createPlayerProfile(uuid);
         PlayerTextures playerTextures = playerProfile.getTextures();
 
         URL urlObject;
         try {
-            urlObject = new URL(url);
+            urlObject = URI.create(url).toURL();
         } catch (MalformedURLException exception) {
             throw new RuntimeException("This URL is incorrect!", exception);
         }
+
         playerTextures.setSkin(urlObject);
         playerProfile.setTextures(playerTextures);
 
