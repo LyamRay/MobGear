75ec93e7036687140ef79502b87ea0ddc470d85b
LyamRay <lyam.ray410@gmail.com>
2024-12-28 18:13:17 +0100

    Updated class.

--- src/main/java/me/lyamray/mobGear/MobGear.java
+++ src/main/java/me/lyamray/mobGear/MobGear.java
@@ -1,11 +1,6 @@
 package me.lyamray.mobGear;
 
 import lombok.Getter;
-import me.lyamray.mobGear.listeners.ForgeBlockListener;
-import me.lyamray.mobGear.listeners.MobKillListener;
-import me.lyamray.mobGear.listeners.PlayerChangeArmorListener;
-import me.lyamray.mobGear.listeners.PlayerJoinListener;
-import org.bukkit.Bukkit;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
 public final class MobGear extends SimplePlugin {
@@ -15,10 +10,10 @@ public final class MobGear extends SimplePlugin {
     public void onPluginStart() {
         instance = this;
         //CustomEnhancedRecipes.registerHeadRecipes();
-        Bukkit.getPluginManager().registerEvents(new MobKillListener(),this);
-        Bukkit.getPluginManager().registerEvents(new ForgeBlockListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerChangeArmorListener(), this);
+        //Bukkit.getPluginManager().registerEvents(new MobKillListener(),this);
+        //Bukkit.getPluginManager().registerEvents(new ForgeBlockListener(), this);
+        //Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        //Bukkit.getPluginManager().registerEvents(new PlayerChangeArmorListener(), this);
     }
 
     public void onPluginStop() {}
